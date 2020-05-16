a1eef13e6a0126ddecd816fb3694892253f409e7
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-05-16 21:54:44 +0400

    Updates

---
+++ precache-manifest.b04eac23fa8c2ea0e59abbc22d9b78ad.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc430b36b892a26e4743aae7750db954",
+    "url": "/spa-example-social-net/index.html"
+  },
+  {
+    "revision": "cd869a2f30821357909f",
+    "url": "/spa-example-social-net/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f178775ace08bcfcadbe",
+    "url": "/spa-example-social-net/static/css/3.ca967c55.chunk.css"
+  },
+  {
+    "revision": "a56838f8aa200968bdf6",
+    "url": "/spa-example-social-net/static/css/4.f47b0be7.chunk.css"
+  },
+  {
+    "revision": "ccbd3aa5c33694f23c34",
+    "url": "/spa-example-social-net/static/css/main.33f74bb4.chunk.css"
+  },
+  {
+    "revision": "cd869a2f30821357909f",
+    "url": "/spa-example-social-net/static/js/2.ba7384a6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spa-example-social-net/static/js/2.ba7384a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f178775ace08bcfcadbe",
+    "url": "/spa-example-social-net/static/js/3.1d888a25.chunk.js"
+  },
+  {
+    "revision": "a56838f8aa200968bdf6",
+    "url": "/spa-example-social-net/static/js/4.423371b4.chunk.js"
+  },
+  {
+    "revision": "ccbd3aa5c33694f23c34",
+    "url": "/spa-example-social-net/static/js/main.de5dde18.chunk.js"
+  },
+  {
+    "revision": "dce3cd84c6a13eee45c5",
+    "url": "/spa-example-social-net/static/js/runtime-main.d9cc94c9.js"
+  },
+  {
+    "revision": "6e93fe924aa7b6d86ec5fcfc9760f09a",
+    "url": "/spa-example-social-net/static/media/userPhoto.6e93fe92.jpg"
+  }
+]);
